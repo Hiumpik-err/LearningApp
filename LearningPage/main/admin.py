e170d2f40313cdcb1783f3e17dec80739531a4e7
Hiumpik-err <kamil.jendrusz@gmail.com>
2025-09-16 16:01:34 +0200

    added database structure

--- LearningPage/main/admin.py
+++ LearningPage/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Uzytkownik)
+admin.site.register(models.Wpis)
+admin.site.register(models.User_Wpis)
--- LearningPage/main/models.py
+++ LearningPage/main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django import forms
 
-# Create your models here.
+class Uzytkownik(models.Model):
+    id_uzytkownika = models.AutoField(primary_key=True)
+    username = models.CharField(max_length = 20)
+    password = models.CharField(max_length= 50)
+    fname = models.CharField(max_length= 20)
+    lname = models.CharField(max_length=50)
+    is_teacher = models.BooleanField(default=False)
+
+    def __str__(self):
+        return username
+
+class Wpis(models.Model):
+    id_wpisu = models.AutoField(primary_key=True)
+    title = models.CharField(max_length = 50)
+    file_name = models.CharField(max_length = 30)
+    subject = models.CharField(max_length = 30)
+    category = models.CharField(max_length = 15)
+
+    def __str__(self):
+        return title
+
+class User_Wpis(models.Model):
+    id_uzytkownika = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)
+    id_wpisu = models.ForeignKey(Wpis, on_delete= models.CASCADE)
+    score = models.IntegerField(default = 0)
+    data = models.DateField(auto_now_add = True)
+
