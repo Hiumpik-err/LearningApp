6bb29cf8138780acd3e1411060e6bd0e240b965b
Hiumpik-err <kamil.jendrusz@gmail.com>
2025-10-14 18:29:07 +0200

    home page updated

--- LearningPage/main/urls.py
+++ LearningPage/main/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path("item/<int:item_id>/", views.item_view, name="item_view"),
     path("create/<str:type>/", views.create_item , name="create_item"),
-    path("filter/", views.search_item, name="search_filter")
 ]
 
 
--- LearningPage/main/views.py
+++ LearningPage/main/views.py
@@ -3,34 +3,16 @@ from . import function as fn
 
 
 def home(request):
-    context = {
-        'card' : {  "id" : 1,
-                    "title": "Quadratic Formula",
-                    "description": "It's fine",
-                    "content" : "Delta"
-                 }
-    }
-    return render(request, "index.html", context)
 
-def item_view(request, item_id):
+    return render(request, "home.html")
 
-    context = {
-        'card' : {  "title": "Quadratic Formula",
-                    "description": "It's fine",
-                    "content" : "Delta"
-                 }
-    }
-    return render(request, "item_view.html", context)
+def item_view(request):
+
+    return render(request, "item_view.html")
 
 def create_item(request, type):
-    if type == "article":
-        fn.create_article(request.POST)
-    elif type == 'task':
-        fn.create_task(request.POST)
-    else:
-        fn.create_quizz(request.POST)
-
-    return render(request, "create_item.html")
+    context = {"type" : type}
+    return render(request, "create_item.html", context)
 
 
 def search_item(request):
