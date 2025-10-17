2053446dc07013a272bff9cffb59765438acdbc6
Hiumpik-err <kamil.jendrusz@gmail.com>
2025-10-17 20:31:12 +0200

    resolved problem with images, links and styles

--- LearningPage/main/urls.py
+++ LearningPage/main/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.home, name="home"),
     path("item/<int:item_id>/", views.item_view, name="item_view"),
     path("create/<str:type>/", views.create_item , name="create_item"),
+    path("available_articles", views.available_article, name="available_articles"),
+    path("available_courses", views.available_courses, name="available_courses"),
+    path("available_quizzes", views.available_quizzes, name="available_quizzes"),
 ]
 
 
--- LearningPage/main/views.py
+++ LearningPage/main/views.py
@@ -14,6 +14,14 @@ def create_item(request, type):
     context = {"type" : type}
     return render(request, "create_item.html", context)
 
+def available_article(request):
+    return render(request, "articles.html")
+
+def available_courses(request):
+    return render(request, "courses.html")
+
+def available_quizzes(request):
+    return render(request, "quizzes.html")
 
 def search_item(request):
     pass
